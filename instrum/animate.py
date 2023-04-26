38df474bf5de4bbae8a6c0258ba3109d4872e683
Daniil <d_tareev@mail.ru>
2023-04-26 08:54:22 +0300

    up

--- instrum/animate.py
+++ instrum/animate.py
@@ -19,13 +19,40 @@ def diagramm(arr, width, nameY):
     plt.show()
 
 
-def graphOfPointsOfDifferentColors(view):
+def graphDiff(root):
     list_current_X = []
     list_current_Y = []
     list_current_Z = []
     list_current_I = []
 
-    with open("outputs/current.txt") as f:
+    with open(root) as f:
+        for line in f:
+            current_line = {}
+            line = line.split()
+            current_line['i'] = line[0].split(':')[1]
+            current_line['x'] = line[1].split(':')[1]
+            current_line['y'] = line[2].split(':')[1]
+            current_line['z'] = line[3].split(':')[1]
+
+            list_current_X.append(float(current_line['x']))
+            list_current_Y.append(float(current_line['y']))
+            list_current_Z.append(float(current_line['z']))
+            list_current_I.append(int(current_line['i']))
+
+    fig = plt.figure(figsize=(12, 12))
+    ax = fig.add_subplot(projection='3d')
+    ax.plot3D(list_current_X, list_current_Y,
+              list_current_Z, color='red', marker=',')
+    plt.show()
+
+
+def graphOfPointsOfDifferentColors(root, view):
+    list_current_X = []
+    list_current_Y = []
+    list_current_Z = []
+    list_current_I = []
+
+    with open(root) as f:
         for line in f:
             current_line = {}
             line = line.split()
@@ -53,7 +80,8 @@ def graphOfPointsOfDifferentColors(view):
             match = False
             while not match:
                 curent_color = '#' + realisation.random_color()
-                match = re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', curent_color)
+                match = re.search(
+                    r'^#(?:[0-9a-fA-F]{3}){1,2}$', curent_color)
             if (view == 'line'):
                 arr[itteration] = ax.plot3D(list_current_X[beginning:end], list_current_Y[beginning:end],
                                             list_current_Z[beginning:end], color=curent_color, marker=',')
@@ -77,6 +105,7 @@ def graphOfPointsOfDifferentColors(view):
     for i in range(len(countlist)):
         countlist[i] = 'photon ' + str(countlist[i] + 1)
 
+    print(photonlist)
     plt.legend(tuple(photonlist), tuple(countlist),
                loc='upper center', ncols=8, fontsize='xx-small')
 
--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from concurrent.futures import ProcessPoolExecutor
 import math
 
 mu_s = 10                   # коэффициентом рассеяния
-mu_a = 0.15                 # коэффициентом поглощения
-g = 0                     # параметр анизатропии
-photons = 100000              # фотоны
-size = 10                  # граница
+mu_a = 0.05                 # коэффициентом поглощения
+g = 0.99                   # параметр анизатропии
+photons = 10000              # фотоны
+size = 100                  # граница
 left = - (size / 2)
 right = (size / 2)
 x_max = 10
@@ -19,21 +19,22 @@ slice = 5
 sliceThinkness = 0.5
 a = 100
 
-masY = [0] * a
-maksY = 0
-for i in range(a):
-    masY[i] = [0] * (a * 2)
+# masY = [0] * a
+# maksY = 0
+# for i in range(a):
+#     masY[i] = [0] * (a * 2)
 
-masZ = [0] * a
-maksZ = 0
-for i in range(a):
-    masZ[i] = [0] * (a * 2)
+# masZ = [0] * a
+# maksZ = 0
+# for i in range(a):
+#     masZ[i] = [0] * (a * 2)
 
 w_arr = []
 for i in range(100):
     w_arr.append(0)
 
 f = open('outputs/current.txt', 'w')
+diff = open('outputs/different_g.txt', 'a')
 
 for i in range(photons):
     # Действительные координаты
@@ -41,13 +42,13 @@ for i in range(photons):
     # Изменение направления фотона
     direction_of_movement = {'Yx': 0, 'Yy': 0, 'Yz': 1}
     w = 1  # Вес фотона
-
     while (left <= current['x'] < right and left <= current['y'] < right and -1 <= current['z'] < 100):
         # Вычисление углов ϕ и θ
         [fi, teta] = realisation.corners(g)
         # Вычисление свободного пробега l
         l = realisation.free_run_l(mu_s, mu_a)
         # Изменение направления движения
+
         [x, y, z] = realisation.changing_the_direction_of_movement(
             direction_of_movement, fi, teta)
 
@@ -60,47 +61,50 @@ for i in range(photons):
         current['z'] = current['z'] + (l * direction_of_movement['Yz'])
         current['i'] = i + 1
 
-        if (current['y'] > -sliceThinkness and current['y'] < sliceThinkness and abs(current['x']) < x_max and abs(current['z']) < x_max and (current['z']) > 0):
-            indeks1 = math.floor(((current['x'] - 10) / x_max) * 100)
-            indeks2 = math.floor((current['z'] / x_max) * 100)
-            masY[indeks2][indeks1] += w
-            if (maksY < masY[indeks2][indeks1]):
-                maksY = masY[indeks2][indeks1]
+        # if i == 20:
+        #     diff.write(
+        #         f"i:{4}  x:{current['x']}  y:{current['y']}  z:{current['z']}\n")
 
-        if (current['z'] > -sliceThinkness and current['z'] < sliceThinkness and abs(current['x']) < x_max and abs(current['y']) < x_max):
-            indeks1 = math.floor(((current['x'] - 10) / x_max) * 100)
-            indeks2 = math.floor(((current['y'] - 10) / x_max) * 50)
-            masZ[indeks2][indeks1] += w
-            if (maksZ < masZ[indeks2][indeks1]):
-                maksZ = masZ[indeks2][indeks1]
+        # if (current['y'] > -sliceThinkness and current['y'] < sliceThinkness and abs(current['x']) < x_max and abs(current['z']) < x_max and (current['z']) > 0):
+        #     indeks1 = math.floor(((current['x'] - 10) / x_max) * 100)
+        #     indeks2 = math.floor((current['z'] / x_max) * 100)
+        #     masY[indeks2][indeks1] += w
+        #     if (maksY < masY[indeks2][indeks1]):
+        #         maksY = masY[indeks2][indeks1]
+
+        # if (current['z'] > -sliceThinkness and current['z'] < sliceThinkness and abs(current['x']) < x_max and abs(current['y']) < x_max):
+        #     indeks1 = math.floor(((current['x'] - 10) / x_max) * 100)
+        #     indeks2 = math.floor(((current['y'] - 10) / x_max) * 50)
+        #     masZ[indeks2][indeks1] += w
+        #     if (maksZ < masZ[indeks2][indeks1]):
+        #         maksZ = masZ[indeks2][indeks1]
 
         if (i <= 50):
             f.write(
                 f"i:{current['i']}  x:{current['x']}  y:{current['y']}  z:{current['z']}\n")
 
         w = realisation.photon_weight(w, mu_s, mu_a)
-        if (w < 0.000001):
+        if (w < 0.0001):
             # print('Фотон поглощен')
             break
 
-        # if (current['z'] < 0):
-        #     answer = math.floor(
-        #         realisation.distanceBetweenPoints(current) / x_max * len(w_arr))
-        #     print(current)
-        #     if (answer < 100):
-        #         w_arr[answer] += w
-        #     break
-
+        if (current['z'] < 0):
+            answer = math.floor(
+                realisation.distanceBetweenPoints(current) / x_max * 100)
+            if (answer < 100):
+                w_arr[answer] += w
+            break
 # График передвижения фотона в среде по точкам
-# animate.graphOfPointsOfDifferentColors('point')
+# animate.graphOfPointsOfDifferentColors('outputs/current.txt', 'point')
 # График передвижения фотона в среде линиями
-# animate.graphOfPointsOfDifferentColors('line')
+animate.graphOfPointsOfDifferentColors('outputs/current.txt', 'line')
+animate.graphOfPointsOfDifferentColors('outputs/different_g.txt', 'line')
 # Гистограмма кол-во фотонов к расстоянию
-# animate.diagramm(w_arr, 0.1, 'count')
+animate.diagramm(w_arr, 0.1, 'count')
 # Гистограмма площадь кольца к расстоянию
-# seq = realisation.calcSquare(w_arr)
-# animate.diagramm(seq, 0.1, 'square')
+seq = realisation.calcSquare(w_arr)
+animate.diagramm(seq, 0.1, 'square')
 
 # vox.voxVisualizerCub(size, array_current)
-vox.voxVisualizer(maksZ, masZ, 'z')
-vox.voxVisualizer(maksY, masY, 'y')
+# vox.voxVisualizer(maksZ, masZ, 'z')
+# vox.voxVisualizer(maksY, masY, 'y')
